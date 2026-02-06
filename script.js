c4bcd57aa67859a7cfd5f697a1c1db7e575b74d0
23x51a3303-create <23x51a3303@srecnandyal.edu.in>
2026-02-06 20:59:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function saveData() {
+
+    let name = document.getElementById("name").value;
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+  
+    let record = {
+      name: name,
+      password: password,
+      email: email,
+      timestamp: new Date().toISOString()
+    };
+  
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+  
+    users.push(record);
+  
+    localStorage.setItem("users", JSON.stringify(users));
+  
+    alert("Data inserted successfully!");
+  
+    document.getElementById("name").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("email").value = "";
+  }
